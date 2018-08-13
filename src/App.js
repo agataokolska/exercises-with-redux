400f4fc9ffe3342e6ed397b9bea8ea7acc6dd05d
agata okolska <agata.okolska@gmail.com>
2018-08-13 13:58:05 +0200

    import combineReducers and createStore from redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Counter from './Counter'
 import './App.css';
 import CounterButtons from './CounterButtons'
+import {combineReducers, createStore} from 'redux'
+
+
+const reducers = combineReducers({
+
+})
+const store = createStore()
 
 class App extends React.Component {
   render() {
