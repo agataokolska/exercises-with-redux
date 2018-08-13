0a3eaefa75aa262485e9697a26d2c5b86b2e013b
agata okolska <agata.okolska@gmail.com>
2018-08-13 12:09:35 +0200

    add store with reducer

---
+++ src/store.js
@@ -0,0 +1,28 @@
+const add = () => (
+    {
+        type: 'ADD'
+    })
+
+const sub = () => (
+    {
+        type: 'SUB'
+    })
+
+const INITIAL_STATE = {
+    counterValue:99,
+    open: false,
+    list: []
+}
+
+const reducer = (state=INITIAL_STATE, action) => {
+    switch (action, type) {
+        case 'ADD':
+            //return {counterValue: state.counterValue + 1, open:false, list:[]}  different methods returning same object
+            //return Object.assign({},state, {counterValue: state.counterValue + 1})
+            return {...state, counterValue: state.counterValue + 1}
+        case 'SUB':
+        return {...state, counterValue: state.counterValue - 1}
+        default:
+            return state
+    }
+}
