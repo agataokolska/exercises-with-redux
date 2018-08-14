1bb4543884bb10b2567dcf197abb58d43d5e20a5
agata okolska <agata.okolska@gmail.com>
2018-08-14 16:25:38 +0200

    add compose

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Counter from './Counter'
 import './App.css';
 import CounterButtons from './CounterButtons'
-import {combineReducers, createStore} from 'redux'
+import {combineReducers, createStore, compose} from 'redux'
 import counterReducer from './store'
 import {Provider} from 'react-redux'
 
@@ -10,7 +10,12 @@ import {Provider} from 'react-redux'
 const reducers = combineReducers({
   counter: counterReducer
 })
-const store = createStore(reducers)
+//const store = createStore(reducers)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const enhancer = composeEnhancers()
+
+const store = createStore(reducers, enhancer)
 
 class App extends React.Component {
   render() {
